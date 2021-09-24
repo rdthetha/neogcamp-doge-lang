720160d848229ea837431dbc6d2b6a985882aee8
rdthetha <rd10072000@gmail.com>
2021-09-24 21:37:42 +0530

    feature

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var btnTranslate= document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+var serverurl= "https://api.funtranslations.com/translate/doge.json";
 
-console.log(txtInput);
 
+function getTranslation(text){
+    return serverurl+ "?text="+text;
+}
+
+function errorHandler(error){
+    console.log("ERROR OCCURED"+ error);
+}
 function clickHandler(){
-    console.log("Clicked");
-    console.log("input",txtInput.value);
+    //outputDiv.innerText=txtInput.value;
+    var inputText=txtInput.value;;
+    fetch(getTranslation(inputText)).then(response=>response.json()).then(json=>outputDiv.innerText=(json.contents.translated)).catch(errorHandler);
 };
 
 btnTranslate.addEventListener("click",clickHandler);
